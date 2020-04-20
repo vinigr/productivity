d15b0d85f604e50e08a7c5d96e35dd777a493ec2
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-04-19 22:34:18 -0300

    chore(server): add jest

---
+++ packages/server/test/environment/mongodb.js
@@ -0,0 +1,45 @@
+const MMS = require('mongodb-memory-server');
+const NodeEnvironment = require('jest-environment-node');
+
+const { default: MongodbMemoryServer } = MMS;
+
+class MongoDbEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config);
+
+    this.mongod = new MongodbMemoryServer({
+      instance: {
+        // dbName: MONGO_DB_NAME,
+      },
+      binary: {
+        version: '4.2.1',
+      },
+      // debug: true,
+      autoStart: false,
+    });
+  }
+
+  async setup() {
+    await super.setup();
+    await this.mongod.start();
+    this.global.__MONGO_URL__ = await this.mongod.getConnectionString();
+    this.global.__MONGO_DB_NAME__ = await this.mongod.getDbName();
+    this.global.__COUNTERS__ = {
+      event: 0,
+    };
+  }
+
+  async teardown() {
+    await super.teardown();
+    // console.error('\n# MongoDB Environment Teardown #\n');
+    await this.mongod.stop();
+    this.mongod = null;
+    this.global = {};
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
+}
+
+module.exports = MongoDbEnvironment;
