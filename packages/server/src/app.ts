eb9b21a9fdb2c6192d2e727dbd0ca3d3328a8bc4
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-04-20 22:57:51 -0300

    feat(server): add graphql hello world

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,8 +2,14 @@ import Koa, { Context } from 'koa';
 import bodyParser from 'koa-bodyparser';
 import cors from 'koa-cors';
 
+import graphqlHttp from 'koa-graphql';
+
 import Router from '@koa/router';
 import logger from 'koa-logger';
+
+import koaPlayground from 'graphql-playground-middleware-koa';
+
+import { schema } from './schema'
 
 const app = new Koa<any, Context>();
 
@@ -19,9 +25,19 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use(bodyParser());
 
+const graphqlServer = graphqlHttp({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+})
+
 router.get('/', (ctx: Context) => {
   ctx.body = 'Hello';
 });
+
+router.all(
+  '/graphql',
+  graphqlServer
+);
 
 app.use(router.routes()).use(router.allowedMethods());
 app.use(cors());
---
+++ packages/server/src/type/QueryType.ts
@@ -0,0 +1,11 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql';
+
+export default new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'Hello'
+    }
+  })
+})
