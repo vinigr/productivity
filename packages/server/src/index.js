ee72fe0ad4d665ea1e020af1452dd2ba5cddc78c
Vinícios Grama <vinyirun1@gmail.com>
2020-05-03 22:37:05 -0300

    feat(server):  connect database
    Co-authored-by: Daniel <danielcostacarvalho@gmail.com>

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,13 +3,23 @@ import { createServer } from 'http';
 import { GRAPHQL_PORT } from './common/config';
 
 import app from './app';
+import connectDatabase from './common/database';
 
-const server = createServer(app.callback());
+(async () => {
+  try {
+    await connectDatabase();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log('Database connection error', err);
+  }
 
-server.listen(GRAPHQL_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.info(`Server started on port: ${GRAPHQL_PORT}`);
-});
+  const server = createServer(app.callback());
+
+  server.listen(GRAPHQL_PORT, () => {
+    // eslint-disable-next-line no-console
+    console.info(`Server started on port: ${GRAPHQL_PORT}`);
+  });
+})();
 
 if (module.hot) {
   module.hot.accept('./index.js', () => {
