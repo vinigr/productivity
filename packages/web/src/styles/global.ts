da488a56f83fb2aad900328260a329bdfb771b0b
Vinícios Grama <vinyirun1@gmail.com>
2020-05-04 17:19:58 -0300

    chore(web): change font import

--- packages/web/src/styles/global.ts
+++ packages/web/src/styles/global.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-
   * {
     margin: 0;
     padding: 0;
