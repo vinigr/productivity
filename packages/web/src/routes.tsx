a7e16ff5825b06d47d5a88044ef61d4b6539c51c
Vinícios Grama <vinyirun1@gmail.com>
2020-05-09 19:30:17 -0300

    chore(web): update dependency react-router-dom

--- packages/web/src/routes.tsx
+++ packages/web/src/routes.tsx
@@ -1,15 +1,11 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-import * as RRD from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const Login = lazy(() => import('./modules/Auth/Login'));
 const Register = lazy(() => import('./modules/Auth/Register'));
 
 const Dashboard = lazy(() => import('./modules/Dashboard/Dashboard'));
 const Home = lazy(() => import('./modules/Home/Home'));
-
-const Routes = (RRD as any).Routes;
 
 const RoutesApp = () => {
   return (
