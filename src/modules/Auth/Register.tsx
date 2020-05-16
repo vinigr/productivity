ac84e772aed6d371b0f7ae4a2ed5da847c12f859
Vinícios Grama <vinyirun1@gmail.com>
2020-05-15 21:26:51 -0300

    feat: create register

--- src/modules/Auth/Register.tsx
+++ src/modules/Auth/Register.tsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
 
 import { Wrapper, Form, Title, Label, Input, Options, LinkChangeAuth, ButtonAdd, ArrowIcon } from './styles';
+import Loading from '../common/Loading';
+import AuthService from '../../services/auth';
+import api from '../../services/api';
 
 const Register = () => {
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const register = async (e: any) => {
+    e.preventDefault();
+
+    if (!username || !email || !password) {
+      return toast('Por favor, preencha todos os campos!', { type: 'warning' });
+    }
+
+    setLoading(true);
+    try {
+      const { data } = await api.post('users', {
+        username,
+        email,
+        password,
+      });
+
+      AuthService.setToken(data.token);
+
+      setLoading(false);
+      return navigate('/');
+    } catch (error) {
+      toast('Erro ao fazer login!', { type: 'error' });
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <Wrapper>
       <Form>
         <Title>Cadastro</Title>
-        <Label>Nome completo</Label>
-        <Input type="text" required />
+        <Label>Usuário</Label>
+        <Input type="text" required value={username} onChange={(e) => setUsername(e.target.value)} />
         <Label>E-mail</Label>
-        <Input type="email" required />
+        <Input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
         <Label>Senha</Label>
-        <Input type="password" required />
+        <Input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
         <Options>
           <LinkChangeAuth to="/login">Fazer login</LinkChangeAuth>
-          <ButtonAdd>
+          <ButtonAdd onClick={register}>
             Cadastrar
             <ArrowIcon />
           </ButtonAdd>
